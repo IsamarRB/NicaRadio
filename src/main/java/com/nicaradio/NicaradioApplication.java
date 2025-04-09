ad13958adb85e92b97b366dc2b570e5168863601
IsaRB <romerobellorini@gmail.com>
2025-04-09 23:44:58 +0200

    Add new branch- main

---
+++ src/main/java/com/nicaradio/NicaradioApplication.java
@@ -0,0 +1,13 @@
+package com.nicaradio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicaradioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicaradioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicaradio/NicaradioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicaradio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicaradioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
