54be627d4d1c6f58f0202081fcc01329a2f1a36a
IsaRB <romerobellorini@gmail.com>
2025-04-10 00:21:41 +0200

    Add code Usuario/Service

---
+++ src/main/java/com/nicaradio/service/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.nicaradio.service;
+
+
+import com.nicaradio.model.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class UsuarioService {
+
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("RadioNicaPU");
+
+    public void crearUsuario(Usuario usuario) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(usuario);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public List<Usuario> obtenerUsuarios() {
+        EntityManager em = emf.createEntityManager();
+        List<Usuario> usuarios = em.createQuery("SELECT u FROM Usuario u", Usuario.class).getResultList();
+        em.close();
+        return usuarios;
+    }
+}
+
