bc483edd7bf9e2c2e77b14f6fb9383eeb1fdc65e
IsaRB <romerobellorini@gmail.com>
2025-04-10 21:51:15 +0200

    Added CantanteRepository code

---
+++ src/main/java/com/nicaradio/repository/CantanteRepository.java
@@ -0,0 +1,12 @@
+package com.nicaradio.repository;
+
+
+import com.nicaradio.model.Cantante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CantanteRepository extends JpaRepository<Cantante, Long> {
+    Cantante findByNombre(String nombre);
+}
+
