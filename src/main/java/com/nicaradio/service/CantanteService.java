cc2f1284ec4a0e53eac109ee96d326c67e9a428d
IsaRB <romerobellorini@gmail.com>
2025-04-10 21:44:12 +0200

    Added CantanteService code

---
+++ src/main/java/com/nicaradio/service/CantanteService.java
@@ -0,0 +1,38 @@
+package com.nicaradio.service;
+
+
+import com.nicaradio.model.Cantante;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CantanteService implements BaseService<Cantante> {
+
+    private final CantanteRepository cantanteRepository;
+
+    public CantanteService(CantanteRepository cantanteRepository) {
+        this.cantanteRepository = cantanteRepository;
+    }
+
+    @Override
+    public List<Cantante> listarTodos() {
+        return cantanteRepository.findAll();
+    }
+
+    @Override
+    public Optional<Cantante> buscarPorId(Long id) {
+        return cantanteRepository.findById(id);
+    }
+
+    @Override
+    public Cantante guardar(Cantante cantante) {
+        return cantanteRepository.save(cantante);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        cantanteRepository.deleteById(id);
+    }
+}
+
