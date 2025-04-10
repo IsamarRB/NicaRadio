4f52cef36a42e28e99b7cfc4f18551a2a7221dbf
IsaRB <romerobellorini@gmail.com>
2025-04-10 20:08:46 +0200

    Added new changes in UsuarioController code

--- src/main/java/com/nicaradio/controller/UsuarioController.java
+++ src/main/java/com/nicaradio/controller/UsuarioController.java
@@ -3,15 +3,14 @@ package com.nicaradio.controller;
 
 import com.nicaradio.model.Usuario;
 import com.nicaradio.service.UsuarioService;
-import org.springframework.http.MediaType;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/usuarios")
 @Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@Consumes(jakarta.ws.rs.core.MediaType.APPLICATION_JSON)
 public class UsuarioController {
 
     private UsuarioService usuarioService = new UsuarioService();
